c1381dc0ccbd24058de32ce064d2b600e2c3e943
decipher07 <social.deej@gmail.com>
2022-11-07 11:10:57 +0530

    🔥 feat: Getting Name and Relation from the data

--- converter.py
+++ converter.py
@@ -10,11 +10,11 @@ def connect_mongodb():
     mydatabase = client['elecvotes']
     mycollection=mydatabase['user']
 
-    rec={
-    'title': 'MongoDB and Python', 
-    'description': 'MongoDB is no SQL database', 
-    'tags': ['mongodb', 'database', 'NoSQL'], 
-    'viewers': 104 
+    rec = {
+        'title': 'MongoDB and Python', 
+        'description': 'MongoDB is no SQL database', 
+        'tags': ['mongodb', 'database', 'NoSQL'], 
+        'viewers': 104
     }
     
     # inserting the data in the database
@@ -22,11 +22,42 @@ def connect_mongodb():
 
     print (rec)
 
+def get_name_relation  ( all_data : list ):
+        sub = '\'s Name'
+        particular_username = [s for s in all_data if sub.lower() in s.lower()][0].split()
+        # print ( particular_username )
+        to_search_father = "Father\'s"
+        to_search_husband = "Husband\'s"
+        to_search_mother = "Mother\'s"
+        relation = "----"
+        user_name = "----"
+
+        for i in range (1, len(particular_username)):
+            if to_search_father in particular_username[i]:
+                user_name = particular_username[i-1]
+                relation = "father"
+            if to_search_husband in particular_username[i]:
+                user_name = particular_username[i-1]
+                relation = "husband"
+            if to_search_mother in particular_username[i]:
+                user_name = particular_username[i-1]
+                relation = "mother"
+
+        return (user_name, relation)
+
+def get_relation_name ( all_data : list ):
+        to_search = "House Number"
+        relation = "undefined"
+        for element in all_data :
+            if to_search in element :
+                relation = (element.split(to_search)[0])
+        
+        return relation
 
 pages = convert_from_path('/home/nopc/Github-Clones/Image-to-text/Mizoram Electoral Votes.pdf')
 
-for i in range (3):
-    pages[i].save('page1.jpg', 'JPEG')
+# for i in range (3):
+#     pages[i].save('page1.jpg', 'JPEG')
 
 image_1 = 'page1.jpg'
 
@@ -45,7 +76,11 @@ for j in range ( 10 ):
         all_data = pytesseract.image_to_string(sample_obj)
         all_data = all_data.replace("\n", " ").strip().split(':')
         print(all_data)
-        print(all_data[-1], all_data[-3])
+        # print(all_data[-1], all_data[-3])
+        name, relation = get_name_relation (all_data)
+        relation_person_name = get_relation_name ( all_data )
+        print ( "Name ", name, " Relation ", relation, " name", relation_person_name )
+        
         top_left_x += 501
         bottom_right_x += 513
     
