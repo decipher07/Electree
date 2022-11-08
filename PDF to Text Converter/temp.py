ca1288ea9a6f58b41e3f2dbcd212f74e57cfa76a
decipher07 <social.deej@gmail.com>
2022-11-08 18:54:11 +0530

    ⚰️ remove: useless code

--- PDF to Text Converter/temp.py
+++
@@ -1,9 +0,0 @@
-import pdfquery
-from pdfquery import *
-from lxml import etree
-
-pdf = pdfquery.PDFQuery('check.pdf')
-pdf.load()
-
-with open('xmltree.xml','wb') as f:
-    f.write(etree.tostring(pdf.tree, pretty_print=True))
